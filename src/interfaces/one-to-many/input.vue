a9d99daf9b121d759f222c80e28b92236d1aa758
Markus Ebenhoeh <mebenhoeh@tripadvisor.com>
2019-10-03 07:19:29 +1000

    pass through all translations data on child collection

--- src/interfaces/one-to-many/input.vue
+++ src/interfaces/one-to-many/input.vue
@@ -453,7 +453,7 @@ export default {
 
               const type = fieldInfo.type.toLowerCase();
 
-              if (type === "json") {
+              if (type === "json" || type === "translation") {
                 delta[key] = after[key];
               }
             });
